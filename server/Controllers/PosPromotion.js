ab1020b524f4813813e96a203fa080f1ab2900e9
bengChu <sukkawat04@gmail.com>
2025-01-27 01:26:57 +0700

    ทดสอบWebHookอีกแล้ว

--- server/Controllers/PosPromotion.js
+++ server/Controllers/PosPromotion.js
@@ -39,6 +39,9 @@ exports.list = async (req, res) => {
     //ลองเติมcommentครั้งที่4...
     s2= "bengtest04";
 
+     //ลองเติมcommentครั้งที่5...
+     s2= "bengtest05";
+
     try {
     const query = {}
     const sort = { Id: -1} //1คือASC, -1คือDESC
